6cd437046985a65ad44f426222831782651b228a
Thomas cadden <tuf96842@temple.edu>
2017-09-13 22:03:26 -0400

    Add files via upload

--- sieveAnalysis.py
+++ sieveAnalysis.py
@@ -1,7 +1,7 @@
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
-plotly.tools.set_credentials_file(username='TomCadden', api_key='O3t8TE5Xl17vsVPTwgSJ')
+
 import xml.etree.ElementTree as ET
 import tkinter as tk
 from tkinter import filedialog
@@ -9,16 +9,32 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
+version = input("Enter the Diggs schema version (eg '2.0.b'):")
+print (version)
+address = '{http://diggsml.org/schemas/' + version +'}'
+address2= '{http://diggsml.org/schemas/' + version +'/geotechnical}'
+user = input("Enter your plotly Username:")
+print (user)
+
+API = input("Enter your plotly API:")
+print (API)
+
+graphName = input("Enter the name for your graoh")
+
+
+plotly.tools.set_credentials_file(username=user, api_key=API)
+
 file_path = filedialog.askopenfilename()
 tree = ET.parse(file_path)
 root = tree.getroot()
 
 
 names=[]
-for test in root.iter('{http://diggsml.org/schemas/2.1.a}Test'):
+
+for test in root.iter(address+'Test'):
     temp=str(test.attrib)
 
-    names.append(temp[56:-2])
+    names.append(temp)
     print(names)
 
 
@@ -26,7 +42,7 @@ sizesArrays = []
 sizes=[]
 i=0
 j=0
-for size in root.iter('{http://diggsml.org/schemas/2.1.a/geotechnical}particleSize'):
+for size in root.iter( address2 + 'particleSize'):
 
     print(i)
     if sizes != []:
@@ -46,7 +62,7 @@ passingArrays = []
 i=0
 j=0
 percentPassing=[]
-for passing in root.iter('{http://diggsml.org/schemas/2.1.a/geotechnical}percentPassing'):
+for passing in root.iter(address2 +'percentPassing'):
     print(i)
     if percentPassing != []:
         if float(passing.text) > percentPassing[len(percentPassing) - 1]:
@@ -83,5 +99,5 @@ layout = dict(title = 'Seive Analysis',
               yaxis = dict(title = 'Percent Passing(%)'),
               )
 fig = dict(data=data,layout=layout)
-py.iplot(fig, filename = 'styled-line')
+py.iplot(fig, filename = graphName)
 
